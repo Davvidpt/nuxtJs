8c4a801f3fe278fb68ac19d88d7fad0ae363428b
Davvidpt <exelandriano@gmail.com>
2023-11-02 17:22:29 +0700

    perbaikan fitur

--- components/cards/CardCart.vue
+++ components/cards/CardCart.vue
@@ -1,14 +1,20 @@
+<script lang="ts" setup>
+const props = defineProps({
+  product: {
+    type: Object,
+    default: {},
+  },
+});
+</script>
+
 <template>
   <div class="bg-white shadow-xl p-6 rounded-xl flex gap-4 items-center">
     <div class="h-[150px] w-[130px] bg-gray-300 rounded-xl p-3">
-      <img
-        src="/images/products/baju-1.png"
-        class="h-full w-full object- contain"
-      />
+      <img :src="props.product.image" class="h-full w-full object-contain" />
     </div>
     <div class="w-full">
-      <h5 class="text-xl font-semibold mb-2">Men's Streetwear Fashion</h5>
-      <p class="text-lg font-light mb-3">$30</p>
+      <h5 class="text-xl font-semibold mb-2">{{ props.product.name }}</h5>
+      <p class="text-lg font-light mb-3">${{ props.product.price }}</p>
       <div class="flex items-center justify-between">
         <div
           class="flex items-center border border-gray-300 w-max rounded- full"
@@ -30,7 +36,8 @@
           </button>
         </div>
         <div
-          class="flex items-center gap-2 border border-gray-300 px-3 py-2 rounded-full cursor-pointer"
+          class="flex items-center gap-2 border border-gray-300 px-3 py-2 rounded- full cursor-pointer"
+          @click="$emit('removeCart', props.product.id)"
         >
           <i class="ri-delete-bin-7-fill text-sm text-gray-600"></i>
           <span class="text-sm text-gray-500">Delete</span>
--- pages/cart.vue
+++ pages/cart.vue
@@ -1,18 +1,44 @@
+<script lang="ts" setup>
+import type { Products } from '~/types/products';
+const products = ref<Products[]>([]);
+const totalPrice = computed(() => {
+return products.value.filter((product) => product.price !==
+undefined).reduce((accumulator, currentValue) => accumulator +
+currentValue.price!, 0);
+})
+onMounted(() => {
+let localStorageData = localStorage.getItem("products");
+if (localStorageData) {
+products.value = JSON.parse(localStorageData);
+}
+})
+const removeCart = (id: number) => {
+products.value = products.value.filter((item) => item.id !== id);
+localStorage.setItem("products", JSON.stringify(products.value));
+}
+</script>
+
 <template>
   <section>
     <div class="container">
       <div class="py-10 flex gap-6">
         <div class="w-[70%]">
-          <div
-            class="flex justify-between items-center pb-7 border-b border- gray-300 mb-6"
-          >
-            <h1 class="text-3xl font-medium">Shopping Cart</h1>
-            <p class="text-3xl font-medium">0 Items</p>
-          </div>
-          <div class="flex flex-col gap-6">
-            <CardsCardCart />
-          </div>
-        </div>
+
+<div class="flex justify-between items-center pb-7 border-b border-gray-
+300 mb-6">
+
+<h1 class="text-3xl font-medium">Shopping Cart</h1>
+<p class="text-3xl font-medium">{{ products.length }} Items</p>
+</div>
+<div v-if="products.length > 0" class="flex flex-col gap-6">
+<template v-for="(item, index) in products" :key="index">
+<CardsCardCart :product="item" @removeCart="removeCart"/>
+</template>
+</div>
+<div v-else>
+<h5 class="text-xl font-light text-center">Cart is empty</h5>
+</div>
+</div>
         <div class="w-[30%] bg-white shadow-xl h-max p-6">
           <h3 class="text-xl font-medium mb-6">Order Summary</h3>
           <div class="flex flex-col gap-3 border-b border-gray-300 pb-4">
